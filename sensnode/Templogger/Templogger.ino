66b6466631f98c84c36ccff6f194a11ff6833159
artekw <arteqw@gmail.com>
2012-03-25 20:33:46 +0200

    templogger

--- sensnode/Templogger/Templogger.ino
+++ sensnode/Templogger/Templogger.ino
@@ -1,64 +1,67 @@
-/*
-  SD card datalogger
- 
- This example shows how to log data from three analog sensors 
- to an SD card using the SD library.
- 	
- The circuit:
- * analog sensors on analog ins 0, 1, and 2
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
- 
- created  24 Nov 2010
- updated 2 Dec 2010
- by Tom Igoe
- 
- This example code is in the public domain.
- 	 
- */
-
 #include <SD.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-const int chipSelect = 10;
+#include <Wire.h> // necessary, or the application won't build properly
+#include <stdio.h>
+#include <PCF8583.h>
+
+#define chipSelect 10 // pin 10 in SensnodeTX
+#define ONE_WIRE_BUS 8
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+int correct_address = 0;
+PCF8583 p (0xA0);
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(10, OUTPUT);
+  sensors.begin();
+//  Serial.print("Initializing SD card...");
+  pinMode(chipSelect, OUTPUT);
   
-  // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
+//    Serial.println("Card failed, or not present");
+
     return;
   }
-  Serial.println("card initialized.");
+//  Serial.println("card initialized.");
 }
 
 void loop()
 {
+  
+  if(Serial.available() > 0){
+       p.year= (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48)) + 2000;
+       p.month = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       p.day = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       p.hour  = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       p.minute = (byte) ((Serial.read() - 48) *10 +  (Serial.read() - 48));
+       p.second = (byte) ((Serial.read() - 48) * 10 + (Serial.read() - 48)); // Use of (byte) type casting and ascii math to achieve result.  
+
+       if(Serial.read() == ';'){
+         Serial.println("setting date");
+	 p.set_time();
+       }
+  }
+  
   // make a string for assembling the data to log:
   String dataString = "";
-
-  // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ","; 
-    }
-  }
-
+  
+  p.get_time();
+  char time[50];
+  char buf[50];
+  sprintf(time, "%02d/%02d/%02d %02d:%02d:%02d",
+	  p.year, p.month, p.day, p.hour, p.minute, p.second);
+  sensors.requestTemperatures(); 
+  
+  dataString = String(time);
+  dataString += ",";  
+  dtostrf(sensors.getTempCByIndex(0), 2, 2, buf);  
+  dataString += buf;
+  
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
@@ -68,12 +71,13 @@ void loop()
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
-    Serial.println(dataString);
+//    Serial.println(dataString);
   }  
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
-  } 
+//    Serial.println("error opening datalog.txt");
+  }
+  delay(5000);
 }
 
 
