98a8e6785ce7343ded36cd1ab6574d0eb5745a4b
Artur Wronowski <arteqw@gmal.com>
2014-10-19 21:48:37 +0200

    konfiguracja w oddzielnym pliku

---
+++ sensnode/sensnodetx/configuration.h
@@ -0,0 +1,26 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#define NODEID              15  // (from 1 to 30)
+#define NETWORK             210
+#define NEW_REV                 // comment for old revision  (3.0 or 3.4)
+
+// Settings
+//#define SMOOTH            3   // smoothing factor used for running averages
+#define PERIOD              1   // minutes
+#define RADIO_SYNC_MODE     2   // http://jeelabs.net/pub/docs/jeelib/RF12_8h.html#a6843bbc70df373dbffa0b3d1f33ef0ae
+
+// Used devices (comment on uncomment if not used)
+#define LDR                    // use LDR sensor
+#define DS18B20                // use 1WIE DS18B20
+
+#define SHT21_SENSOR           // use SHT21
+#define BMP_SENSOR             // use BMP085 or BMP180
+#define DHT_SENSOR             // DHT11 or DHT22
+
+
+#define LED_ON                 // use act led for transmission
+
+//#define DEBUG                  // debug mode - serial output
+
+#endif //__CONFIGURATION_H
--- sensnode/sensnodetx/sensnodetx.ino
+++ sensnode/sensnodetx/sensnodetx.ino
@@ -10,6 +10,7 @@ TODO:
 - http://hacking.majenko.co.uk/node/57
 */
 
+#include "configuration.h"
 // libs for I2C and DS18B20
 #include <Wire.h>
 #include <OneWire.h>
@@ -24,30 +25,6 @@ TODO:
 #include <avr/sleep.h>
 // DHT11 and DHT22 from https://github.com/adafruit/DHT-sensor-library
 #include "DHT.h"
-
-/*************************************************************/
-
-#define NODEID              15  // (from 1 to 30)
-#define NETWORK             210
-#define NEW_REV                 // comment for old revision  (3.0 or 3.4)
-
-// Settings
-//#define SMOOTH            3   // smoothing factor used for running averages
-#define PERIOD              1   // minutes
-#define RADIO_SYNC_MODE     2   // http://jeelabs.net/pub/docs/jeelib/RF12_8h.html#a6843bbc70df373dbffa0b3d1f33ef0ae
-
-// Used devices (comment on uncomment if not used)
-#define LDR                    // use LDR sensor
-#define DS18B20                // use 1WIE DS18B20
-
-#define SHT21_SENSOR           // use SHT21
-#define BMP_SENSOR             // use BMP085 or BMP180
-#define DHT_SENSOR             // DHT11 or DHT22
-
-
-#define LED_ON                 // use act led for transmission
-
-//#define DEBUG                  // debug mode - serial output
 
 /*************************************************************/
 
