1adaf40a743fcf8809d0d8c89ce7da8a54b757be
Artur Wronowski <arteqw@gmal.com>
2014-10-19 21:51:28 +0200

    komentarze

--- sensnode/sensnodetx/configuration.h
+++ sensnode/sensnodetx/configuration.h
@@ -1,15 +1,17 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
+//************ RADIO MODULE *************
 #define NODEID              15  // (from 1 to 30)
 #define NETWORK             210
 #define NEW_REV                 // comment for old revision  (3.0 or 3.4)
 
-// Settings
+//*********** SETTINGS ******************
 //#define SMOOTH            3   // smoothing factor used for running averages
 #define PERIOD              1   // minutes
 #define RADIO_SYNC_MODE     2   // http://jeelabs.net/pub/docs/jeelib/RF12_8h.html#a6843bbc70df373dbffa0b3d1f33ef0ae
 
+//*********** SENSORS ******************
 // Used devices (comment on uncomment if not used)
 #define LDR                    // use LDR sensor
 #define DS18B20                // use 1WIE DS18B20
@@ -18,7 +20,7 @@
 #define BMP_SENSOR             // use BMP085 or BMP180
 #define DHT_SENSOR             // DHT11 or DHT22
 
-
+//*********** OTHER ******************
 #define LED_ON                 // use act led for transmission
 
 //#define DEBUG                  // debug mode - serial output
