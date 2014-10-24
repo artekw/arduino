8fdf9e167306b256c68641e995c94be4dce56568
roblad <rl608742063@gmail.com>
2014-10-24 13:05:08 +0200

    Update configuration.h
    1) refereed to main code 3) Band declaration added as BAND

--- sensnode/sensnodetx/configuration.h
+++ sensnode/sensnodetx/configuration.h
@@ -3,8 +3,9 @@
 
 //************ RADIO MODULE *************
 #define NODEID              1  // (from 1 to 30)
-#define NETWORK             100
+#define NETWORK             210
 #define NEW_REV                 // comment for old revision  (3.0 or 3.4)
+#define BAND RF12_868MHZ // Band
 // #define LOWRATE  
 
 //*********** SETTINGS ******************
@@ -17,8 +18,8 @@
 #define DS_COUNT           3   // TO FIX; NOW ONLY 1 or 3 sensors
 
 //#define SHT21_SENSOR           // use SHT21
-//#define BMP_SENSOR             // use BMP085 or BMP180
-//#define DHT_SENSOR             // DHT11 or DHT22
+#define BMP_SENSOR             // use BMP085 or BMP180
+#define DHT_SENSOR             // DHT11 or DHT22
 #define DHT_SENSOR_TYPE    DHT22
 #define DHT_SENSOR_PORT    P1  // TODO!!
 
@@ -26,7 +27,7 @@
 #define LED_ON                 // use act led for transmission
 
 #define DEV_MODE               // display output only on console, do not send packages via radio
-#define DEBUG                  // debug mode - serial output
+//#define DEBUG                  // debug mode - serial output
 #define DEBUG_BAUD      57600  // if not define baud rate = 9600bps
 
 #endif //__CONFIGURATION_H
